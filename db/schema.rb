fc765d0a6ee2057d700ef61c16ee44580ab8f02e
PlanetEfficacy <jspevack@gmail.com>
2016-12-11 10:40:26 -0700

    Add category to shop

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161211172419) do
+ActiveRecord::Schema.define(version: 20161211173639) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20161211172419) do
     t.integer  "chairs"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.string   "category"
   end
 
 end
