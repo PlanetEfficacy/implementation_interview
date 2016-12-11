d6bd08b3f291f77b98263ccf06cb327a2a3e699b
PlanetEfficacy <jspevack@gmail.com>
2016-12-11 12:38:37 -0700

    Add class methods for chairs dashboard

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,2 +1,25 @@
 class Shop < ApplicationRecord
+  def self.postal_codes
+    all.pluck(:post_code).uniq
+  end
+
+  def self.count_by_post(code)
+    where(post_code: code).count
+  end
+
+  def self.chairs_by_post(code)
+    where(post_code: code).sum(:chairs)
+  end
+
+  def self.percent_chairs_at_post(code)
+    (chairs_by_post(code).to_f / sum(:chairs) * 100).round.to_i
+  end
+
+  def self.place_with_max_chairs_at_post(code)
+    find_by(chairs: maximum(:chairs))
+  end
+
+  def self.max_chairs_at_post(code)
+    where(post_code: code).maximum(:chairs)
+  end
 end
--- spec/models/shop_spec.rb
+++ spec/models/shop_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe Shop, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "gets unique postal_codes" do
+    create(:shop, post_code: "1")
+    create(:shop, post_code: "2")
+    expect(Shop.postal_codes).to eq(["1","2"])
+  end
+
+  it "gets count of places by postal code" do
+    create_list(:shop, 2)
+    count = Shop.count_by_post(Shop.first.post_code)
+    expect(count).to eq(2)
+  end
+
+  it "gets count of chairs by postal code" do
+    create_list(:shop, 2, chairs: 10)
+    count = Shop.chairs_by_post(Shop.first.post_code)
+    expect(count).to eq(20)
+  end
+
+  it "gets the percent of chairs at this postal code" do
+    create(:shop, post_code: "1", chairs: 20)
+    create(:shop, post_code: "2", chairs: 80)
+    percent = Shop.percent_chairs_at_post(Shop.first.post_code)
+    expect(percent).to eq(20)
+  end
+
+  it "gets the place with the most chairs at this postal code" do
+    shop_1 = create(:shop, chairs: 20, post_code: "1")
+    shop_2 = create(:shop, chairs: 80, post_code: "1")
+    place = Shop.place_with_max_chairs_at_post("1")
+    expect(place).to eq(shop_2)
+  end
+
+  it "gets the max chairs at this postal code" do
+    shop_1 = create(:shop, chairs: 20, post_code: "1")
+    shop_2 = create(:shop, chairs: 80, post_code: "1")
+    chairs = Shop.max_chairs_at_post("1")
+    expect(chairs).to eq(80)
+  end
 end
