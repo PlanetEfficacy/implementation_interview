7579954233522406d4e2d2286125cc227b01626c
PlanetEfficacy <jspevack@gmail.com>
2016-12-11 19:53:46 -0700

    Refactor categorizer

--- app/services/categorizer.rb
+++ app/services/categorizer.rb
@@ -5,13 +5,31 @@ class Categorizer
   end
 
   def assign_category!
-    if shop.prefix == "ls1"
-      shop.category = "#{shop.prefix} #{shop.size}"
-    elsif shop.prefix == "ls2"
-      shop.category = "#{shop.prefix} #{shop.small_or_large}"
-    else
-      shop.category = "other"
-    end
+    shop.category = ls1_category if ls1?
+    shop.category = ls2_category if ls2?
+    shop.category = "other" if other?
+    shop.save!
   end
 
+  private
+
+    def ls1?
+      shop.prefix == "ls1"
+    end
+
+    def ls2?
+      shop.prefix == "ls2"
+    end
+
+    def other?
+      !ls1? && !ls2?
+    end
+
+    def ls1_category
+      "#{shop.prefix} #{shop.size}"
+    end
+
+    def ls2_category
+      "#{shop.prefix} #{shop.small_or_large}"
+    end
 end
